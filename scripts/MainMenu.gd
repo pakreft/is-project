18ca6748796080f955d6604f03d5c5f39749c550
lennyb99 <lennogle@gmail.com>
2023-06-25 21:52:13 +0200

    added some menu and new Level

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -8,3 +8,10 @@ func _on_play_button_pressed():
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://_debug/OptionsMenu.tscn")
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://_debug/CreditsMenu.tscn")
+
+
+func _on_shop_pressed():
+	get_tree().change_scene_to_file("res://_debug/ShopMenu.tscn")
